5a20e55f7e22a60b6b21cac984dd52ddb90dfa3e
Marcelo Martins <marcelomartinsdev@gmail.com>
2023-11-23 11:37:54 -0300

    feat: initial setup with Spring Initializer

---
+++ src/test/java/br/com/marcelomartinsdev/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelomartinsdev.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
